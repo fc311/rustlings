b327278c46c3de9c8128774a8e69dac1f928acd4
fc311 <formcollect311@gmail.com>
2025-08-23 20:18:42 +0530

    hashmaps done

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,21 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+
+        fn score_sheet_init() -> TeamScores {
+            TeamScores {
+                goals_scored: 0,
+                goals_conceded: 0,
+            }
+        }
+
+        let team_1_score_sheet = scores.entry(team_1_name).or_insert(score_sheet_init());
+        team_1_score_sheet.goals_scored += team_1_score;
+        team_1_score_sheet.goals_conceded += team_2_score;
+
+        let team_2_score_sheet = scores.entry(team_2_name).or_insert(score_sheet_init());
+        team_2_score_sheet.goals_scored += team_2_score;
+        team_2_score_sheet.goals_conceded += team_1_score;
     }
 
     scores
